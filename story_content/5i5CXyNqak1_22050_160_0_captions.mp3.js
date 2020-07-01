56ca4d755bdf7953a0b5926149e6a128bfe32037
mikestein2016 <mike.stein2016@gmail.com>
2020-06-30 21:40:09 -0400

    ff

---
+++ story_content/5i5CXyNqak1_22050_160_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.41.22450.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:03.947%0D%0ACan%20you%20ever%20use%20information%20from%20the%20Internet%20in%20your%20essay?%0D%0A%0D%0A"}')
---
+++ story_content/61hSqbJuaG1_22050_160_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.41.22450.0%0D%0A%0D%0A00:00:00.072%20--%3E%2000:00:05.561%0D%0ATrue%20or%20False:%20It%E2%80%99s%20okay%20to%20turn%20in%20a%20paper%20that%20you%20wrote%20for%20a%20different%20class.%0D%0A%0D%0A"}')
